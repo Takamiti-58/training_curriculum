23ff0fe1d2fd388a72857e03eb00f1a24f233100
Takamiti-58 <73811747+Takamiti-58@users.noreply.github.com>
2020-11-19 14:45:48 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,6 +8,7 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
+    binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -15,8 +16,10 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
+  # ↑のストロングパラメータのcalendarsに問題がある
+  # requireで指定すべきものが何だったかをおさらいし、正しいものを指定していただきたい
 
   def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
